4ca6309f4cd742b5ce2fecb485116a851c2e36be
DavidU0605 <david.unger@kung.at>
2022-04-08 16:56:57 +0200

    Application Window

--- src/main/java/chess/game/ChessApplication.java
+++ src/main/java/chess/game/ChessApplication.java
@@ -3,18 +3,21 @@ package chess.game;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ChessApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("Hello!");
-//        stage.setScene(scene);
-//        stage.show();
+        GridPane gridpane = new GridPane();
+
+        Scene scene = new Scene(gridpane, 640, 640);
+
+        stage.setTitle("Chess");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
